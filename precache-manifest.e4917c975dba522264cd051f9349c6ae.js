8f4d8d03e9a6392195a833dbe6fa0829fe218680
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-05-25 20:21:47 +0200

    Updates

---
+++ precache-manifest.e4917c975dba522264cd051f9349c6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbe4b1c5e8d277dcc91f191835d3faf",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "c881da6c33ed2527cbf6",
+    "url": "/React-To-Do-App/static/css/main.af666a01.chunk.css"
+  },
+  {
+    "revision": "7371026915a45dbd2244",
+    "url": "/React-To-Do-App/static/js/2.247f3300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-App/static/js/2.247f3300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c881da6c33ed2527cbf6",
+    "url": "/React-To-Do-App/static/js/main.fc0c8ded.chunk.js"
+  },
+  {
+    "revision": "2d5bd235327b501aa8d1",
+    "url": "/React-To-Do-App/static/js/runtime-main.8f151173.js"
+  }
+]);
